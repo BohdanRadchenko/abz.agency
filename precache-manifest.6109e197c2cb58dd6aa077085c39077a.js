0e77438f5902355024ccaf5d488531d1e8bd4a53
BohdanRadchenko <radchenkobs@gmail.com>
2020-02-02 10:45:22 +0200

    Updates

---
+++ precache-manifest.6109e197c2cb58dd6aa077085c39077a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062f3d07594e072608aa231bddbe4ebf",
+    "url": "/abz.agency/index.html"
+  },
+  {
+    "revision": "29c0bca97895caba795e",
+    "url": "/abz.agency/static/css/main.ceb3b464.chunk.css"
+  },
+  {
+    "revision": "17678dd33d5985d9bc23",
+    "url": "/abz.agency/static/js/2.daa63a3f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/abz.agency/static/js/2.daa63a3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29c0bca97895caba795e",
+    "url": "/abz.agency/static/js/main.000be915.chunk.js"
+  },
+  {
+    "revision": "4b4a96cd1c3e1c4a9c81",
+    "url": "/abz.agency/static/js/runtime-main.97689e6f.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abz.agency/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abz.agency/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abz.agency/static/media/man_laptop_v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/abz.agency/static/media/menu_icon.6f6bef28.svg"
+  }
+]);
